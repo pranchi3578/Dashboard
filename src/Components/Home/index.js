03b3d928e520992a5a38f5e5bf4f155928264058
pranchi3578 <joelj4j@gmail.com>
2019-07-26 03:27:58 +0530

    modal

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -1,11 +1,36 @@
 import React, { Component } from "react";
 import { Card, Row } from 'antd';
-import { Button } from 'antd';
+import { Modal , Button } from 'antd';
 
 
 
 export default class index extends Component {
+  state = { visible: false };
+
+  showModal = () => {
+    this.setState({
+      visible: true,
+    });
+  };
+
+  hideModal = () => {
+    this.setState({
+      visible: false,
+    });
+  };
+  confirm(){
+    Modal.confirm({
+      title: 'Confirm',
+      content: 'Bla bla ...',
+      okText: 'ok',
+      cancelText: 'cancel',
+    });
+
+  }
   render() {
+
+ 
+   
    
     return (
       <div>
@@ -20,13 +45,33 @@ export default class index extends Component {
     <Card 
     hoverable
     style={{ width: "600px",height:"600px",boxShadow:"5px 5px #444444"}}>
-        <div style={{width:"500px",height:"550px",
+        <div 
+        style={{width:"500px",height:"550px",
         display:"flex",justifyContent:"flex-end",flexDirection:"column",paddingLeft:"510px"}}>
-      <Button  type="primary" shape="circle" icon="plus" size="large"></Button>
+      <Button 
+       type="primary"
+       shape="circle"
+        icon="plus" 
+        size="large"
+      onClick={this.confirm}
+      ></Button>
       </div>
   </Card>
        </div>
-      
+      <div>
+      <Modal
+      title="Modal"
+      visible={this.state.visible}
+      onOk={this.hideModal}
+      onCancel={this.hideModal}
+      okText="ok"
+      cancelText="cancel"
+    >
+      <p>Bla bla ...</p>
+      <p>Bla bla ...</p>
+      <p>Bla bla ...</p>
+    </Modal>
+      </div>
     
       </div>
     );
