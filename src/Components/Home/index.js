f924637168181ef97afcb699e9b0ca0964ac2c52
pranchi3578 <joelj4j@gmail.com>
2019-07-25 23:36:13 +0530

    first commit

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -1,11 +1,33 @@
 import React, { Component } from "react";
-import { Icon } from "antd";
+import { Card, Row } from 'antd';
+import { Button } from 'antd';
+
+
+
 export default class index extends Component {
   render() {
+   
     return (
       <div>
-        Cj's Starter pack is ready!
-        <Icon type="smile" />
+      <div  style={{
+        display:"flex",
+        justifyContent:"center",
+        backgroundColor:"#000",
+        width:"100vw",
+        height:"100vh",
+        alignItems:"center",
+        flexDirection:"column" }}>
+    <Card 
+    hoverable
+    style={{ width: "600px",height:"600px",boxShadow:"5px 5px #444444"}}>
+        <div style={{width:"500px",height:"550px",
+        display:"flex",justifyContent:"flex-end",flexDirection:"column",paddingLeft:"510px"}}>
+      <Button  type="primary" shape="circle" icon="plus" size="large"></Button>
+      </div>
+  </Card>
+       </div>
+      
+    
       </div>
     );
   }
